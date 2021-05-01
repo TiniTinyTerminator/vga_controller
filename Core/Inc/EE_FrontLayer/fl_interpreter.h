9e6d75393a8a3c85342e1743b4358745e2171490
TiniTinyTerminator <maxbensink@outlook.com>
2021-05-01 15:08:58 +0200

    added some comments

--- Core/Inc/EE_FrontLayer/fl_interpreter.h
+++ Core/Inc/EE_FrontLayer/fl_interpreter.h
@@ -7,6 +7,10 @@
 //          to usable functions and send them to Logic Layer
 /****************************************************************************************************/
 
+// @NOTE gebruike een @ voor de parameter om er een doxygen parameter van te maken, wordt dan meegenomen in de documentatie generator.
+
+// @NOTE vergeet je header define niet!!
+
 #include "stdint.h"
 
 #define SCRIPT_MAX_ARG 10
@@ -17,6 +21,13 @@
 
 uint8_t cmd_in_buf = 0;			//total of script commando's in buffer
 uint8_t last_cmd_in_buf = 0;	//Place of last script commando in buffer
+
+/**
+* @NOTE waarom een short?
+* zorg er trouwens voor dat variabelen zon min mogelijk globaal geplaatst worden
+* zet ze zo nodig locaal in een .c bestand en gebruik dan extern om er vanaf een ander .c bestand bij te komen.
+* zo hou je de variabelen prive zodra je ze compileert
+*/
 
 //storage for scriptline seperated in function and its arguments
 short cmd_buf [CMD_BUF_LEN][SCRIPT_MAX_ARG];
