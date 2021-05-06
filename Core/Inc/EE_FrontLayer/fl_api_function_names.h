89edd37c0e906199630555c70165af72f867e946
Daniel <daniel.mul@student.hu.nl>
2021-05-06 02:49:19 +0200

    updated colors to a total of 8 bit (2b3g3r)

--- Core/Inc/EE_FrontLayer/fl_api_function_names.h
+++ Core/Inc/EE_FrontLayer/fl_api_function_names.h
@@ -26,36 +26,36 @@ typedef struct Functions
 {
 	char name[20];
 	uint8_t argc;
-	Arg_t argt[6];
+	Arg_t argt[7];
 	Apifuncp funcp;
 }Command_t;
 
 typedef struct Colours
 {
 	char name[15];
-	int32_t hexdef;
+	int8_t hexdef;
 }Colour_t;
 
 //according to: https://flaviocopes.com/rgb-color-codes/
 static const Colour_t colour_list[] =
 {
-		{"zwart",		0x000000},
-		{"blauw",		0x0000FF},
-		{"lichtblauw",	0xADD8E6},
-		{"groen",		0x008000},
-		{"lichtgroen",	0x90EE90},
-		{"cyaan",		0x00FFFF},
-		{"lichtcyaan",	0xE0FFFF},
-		{"rood",		0xFF0000},
-		{"lichtrood",	0x800000},
-		{"magenta",		0xFF00FF},
-		{"lichtmagenta",0xEE82EE},
-		{"bruin",		0xA52A2A},
-		{"geel",		0xFF00FF},
-		{"grijs",		0x808080},
-		{"wit",			0xFFFFFF},
-		{"rose",		0xFFC0CB},			//Bonus
-		{"paars",		0x800080},
+		{"zwart",		0b00000000},
+		{"blauw",		0b11000000},
+		{"lichtblauw",	0b10010000},
+		{"groen",		0b00111000},
+		{"lichtgroen",	0b01100000},
+		{"cyaan",		0b11111000},
+		{"lichtcyaan",	0b11110010},
+		{"rood",		0b00000111},
+		{"lichtrood",	0b00000100},
+		{"magenta",		0b00111111},
+		{"lichtmagenta",0x01101101},
+		{"bruin",		0b01010011},
+		{"geel",		0b00111111},
+		{"grijs",		0b10100100},
+		{"wit",			0b11111111},
+		{"rose",		0b01011111},
+		{"paars",		0b10000100},
 		{"END_OF_LIST\0"}
 };
 
@@ -87,9 +87,9 @@ typedef struct Fontstyles
 
 static const Fontstyle_t fontstyle_list[] =
 {
-	{"normaal" , T_NORMAAL},
-	{"vet", T_VET},
-	{"cursief", T_CURSIEF},
+	{"normaal",	T_NORMAAL},
+	{"vet", 	T_VET},
+	{"cursief",	T_CURSIEF},
 	{"END_OF_LIST\0"}
 };
 
